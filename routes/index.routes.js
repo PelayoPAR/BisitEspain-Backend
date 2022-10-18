04c22d9b9fb6eaa8d1f848f2744cb32398c3644e
Maxime Erkens <mhmserkens@gmail.com>
2022-10-18 21:35:43 +0200

    feat: Province list

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const authRoutes = require("./auth.routes");
+const Province = require("../models/Province.model");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  Province.find({}).then((allProvinces) => {
+    console.log(allProvinces);
+    res.json(allProvinces);
+  });
 });
+
+router.use("/auth", authRoutes);
 
 module.exports = router;
