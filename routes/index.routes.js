7d1dadb593c537004ff7935db82f2ab496e17e50
PelayoPAR <pelayopar@gmail.com>
2022-10-22 17:54:10 +0200

    Made Profile Routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth.routes");
+const profileRoutes = require("./profile.routes.js");
 const { Province } = require("../models/Province.model");
 const mongoose = require("mongoose");
 
@@ -12,5 +13,6 @@ router.get("/", (req, res, next) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/profile", profileRoutes);
 
 module.exports = router;
