956c74e5707abfc0a57865c9c3da40a15b7607dd
PelayoPAR <pelayopar@gmail.com>
2022-10-27 16:44:36 +0200

    continue tourist route dynamic render process

--- models/Route.model.js
+++ models/Route.model.js
@@ -1,70 +1,70 @@
 const { Schema, model } = require("mongoose");
 const { commentSchema } = require("./Comment.model");
 
-const routeSchema = new Schema({
-  name: {
-    type: String,
-  },
-  contentType: {
-    type: String,
-    default: "Route",
-  },
-  category: {
-    type: String,
-    enum: ["Urban", "Rural", "Coastal"],
-  },
-  nodes: {
-    type: Array,
-    positions: [
-      {
-        latitude: Number,
-        longitude: Number,
-      },
-    ],
-  },
-  comments: {
-    type: [commentSchema],
-    default: [],
-  },
-});
-
 // const routeSchema = new Schema({
-//   type: {
+//   name: {
 //     type: String,
-//     enum: ["Feature"],
-//     required: true,
 //   },
-//   properties: {
-//     name: {
-//       type: String,
-//     },
-//     contentType: {
-//       type: String,
-//       default: "Route",
-//     },
-//     category: {
-//       type: String,
-//       enum: ["Urban", "Rural", "Coastal"],
-//     },
-//     comments: {
-//       type: [commentSchema],
-//       default: [],
-//     },
+//   contentType: {
+//     type: String,
+//     default: "Route",
 //   },
-//   geometry: {
-//     type: {
-//       type: String,
-//       enum: ["LineString"],
-//       required: true,
-//     },
-//     coordinates: {
-//       type: [Number],
-//       required: true,
-//     },
-//     // type: "LineString",
-//     // coordinates: [[Number, Number]],
+//   category: {
+//     type: String,
+//     enum: ["Urban", "Rural", "Coastal"],
+//   },
+//   nodes: {
+//     type: Array,
+//     positions: [
+//       {
+//         latitude: Number,
+//         longitude: Number,
+//       },
+//     ],
+//   },
+//   comments: {
+//     type: [commentSchema],
+//     default: [],
 //   },
 // });
+
+const routeSchema = new Schema({
+  type: {
+    type: String,
+    // enum: ["Feature"],
+    default: "Feature",
+    required: true,
+  },
+  properties: {
+    name: {
+      type: String,
+    },
+    contentType: {
+      type: String,
+      default: "Route",
+    },
+    category: {
+      type: String,
+      enum: ["Urban", "Rural", "Coastal"],
+    },
+    comments: {
+      type: [commentSchema],
+      default: [],
+    },
+  },
+  geometry: {
+    type: {
+      type: String,
+      // enum: ["LineString"],
+      default: "LineString",
+      // required: true,
+    },
+    coordinates: {
+      type: Array,
+      // required: true,
+    },
+  },
+});
 
 // {
 //   type: "Feature",
