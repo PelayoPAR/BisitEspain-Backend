71a3e05e7af4d272b9099e44a9f95e1cccf3018b
Maxime Erkens <mhmserkens@gmail.com>
2022-10-30 16:32:10 +0100

    Comment has reached the DB!

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const commentRouter = express.Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-const User = require("../models/User.model");
-const Comment = require("../models/Comment.model");
+const { Comment } = require("../models/Comment.model");
+const { Landmark } = require("../models/Landmark.model");
+const { Route } = require("../models/Route.model");
 
 commentRouter.post("/create", isAuthenticated, (req, res) => {
   const { message, rating } = req.body;
 
-  Comment.create({ message, rating, owner })
+  Comment.create({ message, rating, owner: req.payload._id })
     .then((createdComment) => res.json(createdComment))
     .catch((err) => {
       console.log("Could not create Comment");
