bfb62b62d2da4633800e85272df62d9a03da62c7
PelayoPAR <pelayopar@gmail.com>
2022-11-04 23:25:10 +0100

    Update Comment initial implementation, there is an undefined floating in the air

--- config/seeds.js
+++ config/seeds.js
@@ -1,27 +1,29 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require("dotenv").config()
 
 // ℹ️ Connects to the database
-require("../db");
+require("../db")
 
 // Allows to close the DB
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const { Comment } = require("../models/Comment.model");
-const { Landmark } = require("../models/Landmark.model");
-const { Province } = require("../models/Province.model");
-const { Route } = require("../models/Route.model");
-const { User } = require("../models/User.model");
-// const rutaDelCaresCoords = require("./coordinates/rutaDelCarescoords");
+const { Comment } = require("../models/Comment.model")
+const { Landmark } = require("../models/Landmark.model")
+const { Province } = require("../models/Province.model")
+const { Route } = require("../models/Route.model")
+const { User } = require("../models/User.model")
+const { rutaDelCaresCoords } = require("./coordinates/rutaDelCaresCoords.jsx")
+const { rutaPlayasCaboGata } = require("./coordinates/rutaPlayasCaboGata.jsx")
+const { rutaSevillaUno } = require("./coordinates/rutaSevillaUno.jsx")
 
 const seedDB = async () => {
   // Clear previous DB contents:
   //   console.log(Landmark);
-  await Landmark.deleteMany({});
-  await Route.deleteMany({});
-  await Province.deleteMany({});
-  await Comment.deleteMany({});
+  await Landmark.deleteMany({})
+  await Route.deleteMany({})
+  await Province.deleteMany({})
+  await Comment.deleteMany({})
 
   const sevilleLandmarks = await Landmark.create([
     {
@@ -42,7 +44,7 @@ const seedDB = async () => {
       position: { latitude: 37.37729, longitude: -5.986878 },
       URL: "https://en.wikipedia.org/wiki/Plaza_de_Espa%C3%B1a,_Seville",
     },
-  ]);
+  ])
 
   const sevilleRoutes = await Route.create([
     {
@@ -50,62 +52,10 @@ const seedDB = async () => {
       properties: { name: "Seville Test Route", category: "Urban" },
       geometry: {
         type: "LineString",
-        coordinates: [
-          [-5.9939, 37.38539],
-          [-5.99363, 37.38483],
-          [-5.99358, 37.38446],
-          [-5.99356, 37.38431],
-          [-5.99356, 37.3841],
-          [-5.99377, 37.384],
-          [-5.99385, 37.38394],
-          [-5.99403, 37.38372],
-          [-5.9943, 37.38364],
-          [-5.99549, 37.3833],
-          [-5.99641, 37.38299],
-          [-5.99656, 37.38312],
-          [-5.99674, 37.38301],
-          [-5.9962, 37.38244],
-          [-5.99593, 37.38209],
-          [-5.99567, 37.3822],
-          [-5.99568, 37.38222],
-          [-5.99556, 37.38228],
-          [-5.99461, 37.38217],
-          [-5.99453, 37.38217],
-          [-5.99446, 37.38219],
-          [-5.99437, 37.38216],
-          [-5.99433, 37.38213],
-          [-5.99401, 37.38212],
-          [-5.99397, 37.38215],
-          [-5.99393, 37.3822],
-          [-5.99365, 37.38217],
-          [-5.99354, 37.38212],
-          [-5.99339, 37.38192],
-          [-5.99339, 37.38184],
-          [-5.9937, 37.38104],
-          [-5.99282, 37.38082],
-          [-5.99271, 37.38075],
-          [-5.99267, 37.38064],
-          [-5.99263, 37.38028],
-          [-5.99255, 37.38015],
-          [-5.99241, 37.38005],
-          [-5.9919, 37.37992],
-          [-5.99009, 37.37945],
-          [-5.98959, 37.3793],
-          [-5.98959, 37.37919],
-          [-5.98954, 37.37913],
-          [-5.98943, 37.37906],
-          [-5.98933, 37.37904],
-          [-5.98924, 37.37904],
-          [-5.98911, 37.37884],
-          [-5.98907, 37.37875],
-          [-5.98897, 37.3788],
-          [-5.98886, 37.37872],
-          [-5.98857, 37.37824],
-          [-5.98785, 37.37706],
-        ],
+        coordinates: rutaSevillaUno,
       },
     },
-  ]);
+  ])
 
   //   console.log(sevilleLandmarks);
 
@@ -125,7 +75,7 @@ const seedDB = async () => {
       category: "Coastal",
       position: { latitude: 36.95279270224342, longitude: -1.898618720458036 },
     },
-  ]);
+  ])
 
   const almeriaRoutes = await Route.create([
     {
@@ -133,14 +83,10 @@ const seedDB = async () => {
       properties: { name: "Almeria Test Route", category: "Coastal" },
       geometry: {
         type: "LineString",
-        coordinates: [
-          [-1.898618720458036, 36.95279270224342],
-          [-2.0064208333557585, 36.86076969276527],
-          [-2.1221465827692327, 36.74477240126721],
-        ],
+        coordinates: rutaPlayasCaboGata,
       },
     },
-  ]);
+  ])
   // const almeriaRoutes = await Route.create([
   //   {
   //     name: "Almeria Test Route",
@@ -174,7 +120,7 @@ const seedDB = async () => {
       position: { latitude: 43.255247319773346, longitude: -4.836736939292379 },
       URL: "https://en.wikipedia.org/wiki/Cares_Trail",
     },
-  ]);
+  ])
   //   console.log(asturiasLandmarks);
 
   const asturiasRoutes = await Route.create([
@@ -183,14 +129,10 @@ const seedDB = async () => {
       properties: { name: "Ruta del Cares", category: "Rural" },
       geometry: {
         type: "LineString",
-        coordinates: [
-          [-4.905537303433168, 43.213300401765764],
-          [-4.88370792600688, 43.24853601425979],
-          [-4.830832011063734, 43.258285717408214],
-        ],
+        coordinates: rutaDelCaresCoords,
       },
     },
-  ]);
+  ])
   // const asturiasRoutes = await Route.create([
   //   {
   //     name: "Ruta del Cares",
@@ -241,12 +183,12 @@ const seedDB = async () => {
         longitude: -5.489437103582958,
       },
     },
-  ];
+  ]
 
-  await Province.create(provinces);
-};
+  await Province.create(provinces)
+}
 
 seedDB().then(() => {
-  console.log("Sedding succesful, closing DB connection");
-  mongoose.connection.close();
-});
+  console.log("Sedding succesful, closing DB connection")
+  mongoose.connection.close()
+})
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const commentRouter = express.Router();
-const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-const { Comment } = require("../models/Comment.model");
-const { Landmark } = require("../models/Landmark.model");
-const { Route } = require("../models/Route.model");
+const express = require("express")
+const commentRouter = express.Router()
+const { isAuthenticated } = require("../middleware/jwt.middleware.js")
+const { Comment } = require("../models/Comment.model")
+const { Landmark } = require("../models/Landmark.model")
+const { Route } = require("../models/Route.model")
 
 commentRouter.post("/create", isAuthenticated, async (req, res) => {
-  const { message, rating, isLandmark, _id: touristicItemId } = req.body;
+  const { message, rating, isLandmark, _id: touristicItemId } = req.body
 
   try {
     const createdComment = await Comment.create({
       message,
       rating,
       owner: req.payload._id,
-    });
+    })
     if (isLandmark) {
       const updatedLandmark = await Landmark.findByIdAndUpdate(
         touristicItemId,
@@ -21,8 +21,8 @@ commentRouter.post("/create", isAuthenticated, async (req, res) => {
           $push: { comments: createdComment._id },
         },
         { new: true }
-      );
-      res.json({ updatedLandmark, createdComment }); // To update the comment list in UI
+      )
+      res.json({ updatedLandmark, createdComment }) // To update the comment list in UI
     } else {
       const updatedRoute = await Route.findByIdAndUpdate(
         touristicItemId,
@@ -30,11 +30,11 @@ commentRouter.post("/create", isAuthenticated, async (req, res) => {
           $push: { "properties.comments": createdComment._id },
         },
         { new: true }
-      );
-      res.json({ updatedRoute, createdComment });
+      )
+      res.json({ updatedRoute, createdComment })
     }
   } catch (err) {
-    console.log("Could not create Comment. Error: ", err);
+    console.log("Could not create Comment. Error: ", err)
   }
 
   // Comment.create({ message, rating, owner: req.payload._id })
@@ -42,49 +42,64 @@ commentRouter.post("/create", isAuthenticated, async (req, res) => {
   //   .catch((err) => {
   //     console.log("Could not create Comment");
   //   });
-});
+})
 
 commentRouter.get("/:touristicItemType/getComments", (req, res) => {
-  const { touristicItemType } = req.params;
-  const { touristicItemId } = req.query;
+  const { touristicItemType } = req.params
+  const { touristicItemId } = req.query
 
-  console.log({ touristicItemId, touristicItemType });
+  console.log({ touristicItemId, touristicItemType })
 
   if (touristicItemType === "landmark") {
-    return Landmark.findById(touristicItemId)
-      .populate("comments")
+    Landmark.findById(touristicItemId)
+      .populate({
+        path: "comments",
+        model: "Comment",
+        populate: {
+          path: "owner",
+          model: "User",
+          select: "-password",
+        },
+      })
       .then(({ comments }) => {
-        res.json({ comments });
-      });
+        res.json({ comments })
+      })
   } else if (touristicItemType === "route") {
-    return Route.findById(touristicItemId)
-      .populate("properties.comments")
+    Route.findById(touristicItemId)
+      .populate({
+        path: "properties.comments",
+        model: "Comment",
+        populate: {
+          path: "owner",
+          model: "User",
+          select: "-password",
+        },
+      })
       .then(({ properties: { comments } }) => {
-        res.json({ comments });
-      });
+        res.json({ comments })
+      })
   } else {
-    res.status(400).json({ err: "Touristic Item Type not supported" });
+    res.status(400).json({ err: "Touristic Item Type not supported" })
   }
-});
+})
 
-commentRouter.put(
-  "/:touristicItemType/updateComments",
-  isAuthenticated,
-  (req, res) => {
-    const { message, rating } = req.body;
-    const { touristicItemType } = req.params;
-    const { touristicItemId } = req.query;
-
-    // if (touristicItemType === "landmark") {
-    //   return Landmark.findById(touristicItemId).updateOne(), {
-    //     message,
-    //     rating,
-    //     owner: req.payload._id,
-    //   })
-    //     .then(() => {})
-    //     .catch(() => {});
-    // }
+commentRouter.put("/:commentId", isAuthenticated, async (req, res) => {
+  const { commentId } = req.params
+  const { message, rating } = req.body
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        message: message,
+        rating: rating,
+      },
+      { new: true }
+    )
+    res.json(updatedComment)
+  } catch (err) {
+    console.error("Comment edit failed", err)
+    res.status(400).json({ err: "Comment edit failed" })
   }
-);
+})
 
-module.exports = commentRouter;
+module.exports = commentRouter
