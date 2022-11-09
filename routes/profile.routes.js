f3dfd47bd25eea42dd449d06f4e16b7059f0c7d2
PelayoPAR <pelayopar@gmail.com>
2022-11-10 00:36:06 +0100

    commit for consistency

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const router = express.Router()
-const { isAuthenticated } = require("../middleware/jwt.middleware.js")
-const User = require("../models/User.model")
+const express = require("express");
+const router = express.Router();
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+const { commentSchema } = require("../models/Comment.model.js");
+const User = require("../models/User.model");
 
 router.delete("/:userId", isAuthenticated, (req, res, next) => {
   User.findByIdAndDelete(req.params.userId)
     .then(() => {
       res.status(200).json({
         message: "Your profile is succesfully deleted. Sorry to see you go.",
-      })
+      });
     })
     .catch((err) => {
-      console.log("Don't delete what is not yours, very naughty!")
-    })
-})
+      console.log("Don't delete what is not yours, very naughty!");
+    });
+});
 
-module.exports = router
+module.exports = router;
